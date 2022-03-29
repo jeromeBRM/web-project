33ce897627549c07a3321dfe989d2794275fecc8
amemelili <91413456+amemelili@users.noreply.github.com>
2022-03-29 22:34:45 +0200

    Create database user

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('./db/main.sqlite', (err) => {
+  if(err){
+    return console.error(err.message);
+  }
+
+  console.log('Connected to the main.sqlite database.');
+});
+
+db.close((err) =>{
+  if(err){
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
 });
 
 module.exports = app;
