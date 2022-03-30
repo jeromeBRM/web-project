47f666b5a32e4bb8bfeda72443c6ebcecde7ed06
jeromeBRM <54836707+jeromeBRM@users.noreply.github.com>
2022-03-30 13:34:42 +0200

    components

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,14 +2,26 @@ import '../App.css';
 
 function Signup() {
   return (
-    <div className="app" lang="fr">
-      <header className="App-header">
-      </header>
-      <main className="App-main">
-        Créer un compte
-      </main>
-      <footer className="App-footer">
-      </footer>
+    <div className="menu__form--wrapper">
+      <form action="" method="post" className="form--wrapper">
+        <div className="form__item--wrapper">
+          <label for="email">Adresse e-mail</label>
+          <input type="email" name="email" id="email" required className="form__text--input" placeholder="Adresse e-mail"/>
+        </div>
+        <div className="form__item--wrapper">
+          <label for="password">Mot de passe</label>
+          <input type="password" name="password" id="password" required className="form__text--input" placeholder="Mot de passe"/>
+        </div>
+        <div className="form__item--wrapper">
+          <label for="password">Répéter le mot de passe</label>
+          <input type="password" name="password" id="password" required className="form__text--input" placeholder="Répéter le mot de passe"/>
+        </div>
+        <div className="form__item--wrapper">
+          <div className="form__input--wrapper">
+            <input type="submit" value="Inscription" className="form__btn--orange"/>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
