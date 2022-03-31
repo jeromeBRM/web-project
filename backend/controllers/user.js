9369723aaed6e5b77691855f0bf5ecfa48c75280
amemelili <91413456+amemelili@users.noreply.github.com>
2022-03-31 16:15:15 +0200

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 const db = require('../database').db;
 
-exports.get = () => {
+exports.get = (req, res, next) => {
     db.each('select * from user' , (err, data) => {
         if(err){
           return console.error(err.message);
@@ -11,22 +11,15 @@ exports.get = () => {
 }
 
 exports.signup = (req, res, next) => {
-    console.log(req.body);
-    /*bcrypt.hash(req.body.password, 10)
+    bcrypt.hash(req.body.password, 10)
       .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
+        db.run('insert into user (email, password) values (?,?)',[req.body.email,hash])
           .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
-      */
-      db.run('insert into user (email, password) values (?,?)',[req.body.email,req.body.password])
   };
-
+  
 exports.login = (req, res, next) => {
 
 };
