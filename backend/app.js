5274ae1e937b496c2dd214227102323bc0a0bd92
amemelili <91413456+amemelili@users.noreply.github.com>
2022-03-31 15:55:28 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const app = express();
 
 const userRoutes = require('./routes/user');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 //app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
