9846bbeec9152301d7c6f9d4941595c31a199969
amemelili <91413456+amemelili@users.noreply.github.com>
2022-03-31 23:58:03 +0200

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const userCtrl = require('../controllers/user');
 
-router.get('/', userCtrl.get);
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.get('/', auth, userCtrl.getDatabase);
+router.post('/signup', auth, userCtrl.signup);
+router.post('/login', auth, userCtrl.login);
 
 module.exports = router;
