9e283ea457d1d735a9617774dd0eab8a4e354dd9
amemelili <91413456+amemelili@users.noreply.github.com>
2022-04-02 16:14:16 +0200

    Create database.sql

---
+++ backend/db/database.sql
@@ -0,0 +1,27 @@
+create table user ( 
+	id integer , 
+	email text unique not null, 
+	password text not null,
+	verifed integer not null,
+	url_verification text, 
+	constraint pk_user primary key (id)
+);
+
+create table list (
+    id integer,
+    user_id integer not null,
+    description text not null,
+    constraint pk_list primary key (id),
+    constraint fk_user foreign key (user_id) references user(id)
+);
+
+create table task (
+    id integer,
+    list_id integer not null,
+    title text not null,
+    description text,
+    deadline date,
+    completed interger not null,
+    constraint pk_task primary key (id),
+    constraint fk_list foreign key (list_id) references list(id)
+);
