2c9b56b361d83d14c57af4b6b17b211c3baa6cb6
amemelili <91413456+amemelili@users.noreply.github.com>
2022-04-03 02:48:21 +0200

    create task

---
+++ backend/controllers/task.js
@@ -0,0 +1,59 @@
+const db = require('../database').db;
+
+
+// lire les tasks d'une liste specifier par son id 
+exports.getTasksList = (req, res, next) => {
+    db.all('select * from task where list_id = ?', [req.query.list_id], (err,data) => {
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            console.log(data);
+            res.status(201).json(data)
+        }
+    })
+}
+ // but recup toute les taches qui possede une deadline et qui appartienne a une liste creer par un utilisateur donc faire jointure
+exports.getTasksDeadline = (req, res, next) => {
+    db.run('select * from task inner join list on task.list_id = list.id where user_id = ? and task.deadline != null', [req.body.user_id], (err,data) => {
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            console.log(data);
+            res.status(201).json(data)
+        }
+    })
+}
+
+exports.create = (req, res, next) => {  
+    db.run('insert into task (list_id, title, description, deadline, completed) values (?,?,?,?,0)',[req.body.list_id , req.body.title, req.body.description], (err) => { 
+    
+        if (err) {
+            res.status(400).json({ err });
+        }
+         else
+            res.status(201).json({ message: ' Tâche créée !' });
+    })
+}
+
+exports.delete = (req, res, next) => {  
+    db.run('delete from task where id = ?',[req.body.id], (err) => { 
+    
+        if (err) {
+            res.status(400).json({ err });
+        }
+         else
+            res.status(201).json({ message: 'Tâche effacée !' });
+    })
+}
+
+exports.update = (req, res, next) => {
+    db.run('update task set title = ? , description = ?, deadline = ?, completed = 1  where id = ?',[req.body.title, req.body.description, req.body.deadline, req.body.id], (err) => {
+      if (err) {
+        res.status(400).json({ err });
+      }
+      else
+        res.status(201).json({ message: 'Tâche modifiée' });  
+    })
+  }
--- backend/routes/task.js
+++ backend/routes/task.js
@@ -2,3 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const taskCtrl = require('../controllers/task');
+
+router.get('/', taskCtrl.getTasksList);
+router.post('/TasksDeadline', taskCtrl.getTasksDeadline);
+router.post('/create', taskCtrl.create);
+router.post('/delete', taskCtrl.delete);
+router.post('/update',taskCtrl.update);
+
+
+module.exports = router;
