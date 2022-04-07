20f79966e84bf31f12662d171fc29e742376b07f
amemelili <91413456+amemelili@users.noreply.github.com>
2022-04-07 22:21:27 +0200

    Forgotten password

--- frontend/src/pages/ForgottenPassword.jsx
+++ frontend/src/pages/ForgottenPassword.jsx
@@ -5,9 +5,20 @@ import React, { useState } from 'react';
 
 function ForgottenPassword() {
     const [emailState, setEmailValue] = useState({ value:"", changed:false });
+    const [emailfeedback, setEmailFeedback] = useState("");
+    const [formSubmitState, setFormState] = useState(true);
 
     const submitForgottenPassword = async (e) => {
     e.preventDefault();
+
+    if (!emailState.value.includes("@") || emailState.value === ""){
+         setEmailFeedback("Veuillez saisir une adresse e-mail");
+         return;
+    }
+    else if(emailState.value.includes("@")){
+        setEmailFeedback("Si cette e-mail correspond à un compte existant, un mail a été envoyé");
+    } 
+    
         
     await fetch("http://localhost:4200/api/auth/resetPassword", {
       body: JSON.stringify({
@@ -23,8 +34,8 @@ function ForgottenPassword() {
   return (
     <div className="menu__form--wrapper">
         <form action="" method="post" className="form--wrapper">
-        <Input type="email" label = "Adresse e-mail" val="email" required={ true } onchange={ (e) => { setEmailValue({ value:e.target.value, changed:true }) } } feedback={ "" } ></Input>
-        <Button label ="Envoyer" onclick={ (e) => { submitForgottenPassword(e) } } active={ true } ></Button>
+        <Input type="email" label = "Adresse e-mail" val="email" required={ true } onchange={ (e) => { setEmailValue({ value:e.target.value, changed:true }) } } feedback={ emailfeedback } ></Input>
+        <Button label ="Envoyer" onclick={ (e) => { submitForgottenPassword(e) } } active={ formSubmitState } ></Button>
         </form>
     </div>
   );
