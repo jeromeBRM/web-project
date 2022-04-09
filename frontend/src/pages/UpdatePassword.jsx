be13a6150200a72d7df306cb9bcbb7cedbef2974
amemelili <91413456+amemelili@users.noreply.github.com>
2022-04-09 16:48:01 +0200

    Update UpdatePassword.jsx

--- frontend/src/pages/UpdatePassword.jsx
+++ frontend/src/pages/UpdatePassword.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import '../App.css';
 import Button from '../components/Button';
 import Input from '../components/Input';
@@ -9,13 +9,18 @@ function  UpdatePassword(props) {
 
   const [newPasswordState, setNewPasswordValue] = useState({ value:"", changed:false });
   const [newPasswordRepeatState, setNewPasswordRepeatValue] = useState({ value:"", changed:false });
-  
+  const [showLink, setShowLink] = useState(false);
+  const toggleShowLink = () => {
+    if (newPasswordState.value === newPasswordRepeatState.value)
+    setShowLink((showLink) => !showLink)
+  }
   const { url } = useParams();
     
   const submitPassword = async (e) => {
     e.preventDefault();
-  if (newPasswordState.value !== newPasswordRepeatState.value || newPasswordRepeatState.value === "" || newPasswordState.value === "")
-    return;
+
+    if (newPasswordState.value !== newPasswordRepeatState.value || newPasswordRepeatState.value === "" || newPasswordState.value === "")
+      return;
 
   await fetch("http://localhost:4200/api/auth/updatePassword?url_verification="+url, {
     body: JSON.stringify({
@@ -34,10 +39,11 @@ function  UpdatePassword(props) {
         <form action="" method="post" className="form--wrapper">
         <Input type="password" label = "Nouveau mot de passe" val="new-password" required={true} onchange={ (e) => setNewPasswordValue({value:e.target.value, changed:true}) } feedback={ "" } ></Input>
         <Input type="password" label = "Confirmer le nouveau mot de passe" val="new-password-repeat" required={true} onchange ={ (e) => setNewPasswordRepeatValue({value:e.target.value, changed:true}) } feedback={ "" } ></Input>
-        <Button label ="Modifier le mot de passe" onclick= { (e) => {submitPassword(e)} } active={ true } ></Button> 
+        <Button label ="Modifier le mot de passe" onclick= { (e) => { submitPassword(e); toggleShowLink() }} active={ true } ></Button> 
+        {showLink ? <Link to="/signin" >Le mot de passe a été modifier avec succès. Cliquez ici pour vous connecter</Link> : null}
         </form>
     </div>
   );
 }
 
-export default UpdatePassword; 
+export default UpdatePassword;
